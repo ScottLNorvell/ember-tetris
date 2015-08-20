99e2476a900e70538e7052459e17d1b7d8468cb1
Scott Norvell <scottlnorvell@gmail.com>
2015-08-20 08:59:42 -0400

    Make all the tetrominos

--- app/components/ttr-board.js
+++ app/components/ttr-board.js
@@ -1,6 +1,8 @@
 import Ember from 'ember';
 
 const increment = 30;
+const tetrominoTypes = ['i', 'o', 't', 'z', 's', 'j', 'l'];
+let ttrI = 0;
 export default Ember.Component.extend({
   classNames: ['tetris-board'],
   tagName: 'svg',
@@ -9,6 +11,7 @@ export default Ember.Component.extend({
   height: '660px',
   xPos: 0,
   yPos: 0,
+  tetrominoType: 'i',
   downRect() {
     this.set('yPos', this.get('yPos') + increment);
   },
@@ -18,8 +21,13 @@ export default Ember.Component.extend({
   leftRect() {
     this.set('xPos', this.get('xPos') - increment);
   },
+  changeTetromino() {
+    ttrI = (ttrI + 1) % tetrominoTypes.length;
+    this.set('tetrominoType', tetrominoTypes[ttrI]);
+  },
   didInsertElement() {
     Ember.$(document).on('keypress', (e) => {
+      e.preventDefault();
       console.log(e.keyCode);
       if (e.keyCode === 115) {
         this.downRect();
@@ -27,6 +35,8 @@ export default Ember.Component.extend({
         this.leftRect();
       } else if (e.keyCode === 100) {
         this.rightRect();
+      } else if (e.keyCode === 108) {
+        this.changeTetromino();
       }
     });
   }
--- app/components/ttr-tetromino.js
+++ app/components/ttr-tetromino.js
@@ -2,18 +2,36 @@ import Ember from 'ember';
 
 const {
   Component,
-  computed
+  computed,
+  inject
 } = Ember;
 
 export default Component.extend({
+  tetromino: inject.service(),
   tagName: 'g',
   attributeBindings: ['style'],
+  scale: 30,
   style: computed('translateX', 'translateY', function() {
     let {
       translateY,
       translateX
     } = this;
-    return "transform: translate("+ translateX +"px, "+ translateY +"px)";
+    return `transform: translate(${translateX}px, ${translateY}px)`;
   }),
-  yPositions: [0,30,60,90]
+  positions: computed('type', 'scale', function() {
+    let {
+      scale,
+      type
+    } = this;
+    let data = this.get(`tetromino.${type}`);
+    let positions = [];
+    for (let i = 0, len = data[0].length; i < len; i++) {
+      let position = {
+        x: data[0][i] * scale,
+        y: data[1][i] * scale
+      }
+      positions.push(position);
+    }
+    return positions;
+  })
 });
