4853976d4f64db04352cfc3da6a0254df9e5d8e8
Scott Norvell <scottlnorvell@gmail.com>
2015-08-28 18:18:17 -0400

    Implement classic tetris scoring...

--- app/services/scoring.js
+++ app/services/scoring.js
@@ -5,21 +5,24 @@ const {
   computed
 } = Ember;
 
+const baseScores = [40, 100, 300, 1200];
+
 export default Service.extend({
   score: 0,
   lines: 0,
   level: computed('lines', function() {
     let lines = this.get('lines');
     if (lines) {
-      return Math.ceil(lines/5)
+      return Math.floor(lines/5)
     } else {
-      return 1;
+      return 0;
     }
   }),
   addScore(lines) {
     let linesCleared = lines.length;
-    // TODO: make an exponential scale based on number of lines and level
-    this.incrementProperty('score', linesCleared * 100);
+    let level = this.get('level');
+    let score = baseScores[linesCleared - 1] * (level + 1);
+    this.incrementProperty('score', score);
     this.incrementProperty('lines', linesCleared);
   }
 });
