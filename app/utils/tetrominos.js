a31891d277c404960de1dd9872ae0bc1dee3a0f5
Scott Norvell <scottlnorvell@gmail.com>
2015-10-06 08:59:58 -0400

    Dry up tetrominos util
    just cause we can...

--- app/utils/tetrominos.js
+++ app/utils/tetrominos.js
@@ -1,58 +1,73 @@
+import Ember from 'ember';
+
+const { merge } = Ember;
+
+function defaultAttrs() {
+  return {
+    box: [3,3],
+    origin: 'center',
+    rotationOrigin: [1,1],
+    rotations: 4,
+    nextCenter: [1, 1.5]
+  };
+}
+
 export default {
-  t: {
-    positions: [[1,0,1,2], [0,1,1,1]],
-    box: [3,3],
-    origin: 'center',
-    rotationOrigin: [1,1],
-    rotations: 4,
-    nextCenter: [1,1.5]
-  },
-  s: {
-    positions: [[1,2,0,1], [0,0,1,1]],
-    box: [3,3],
+  t: merge(defaultAttrs(), {
+      positions: [
+        [1,0,1,2],
+        [0,1,1,1]
+      ]
+    }),
+  s: merge(defaultAttrs(), {
+    positions: [
+      [1,2,0,1],
+      [0,0,1,1]
+    ],
     origin: [2,1],
     rotationOrigin: [1.5, 0.5],
     rotations: 2,
-    nextCenter: [1,1.5]
-  },
-  z: {
-    positions: [[0,1,1,2], [0,0,1,1]],
-    box: [3,3],
+  }),
+  z: merge(defaultAttrs(), {
+    positions: [
+      [0,1,1,2],
+      [0,0,1,1]
+    ],
     origin: [2,1],
     rotationOrigin: [1.5, 0.5],
     rotations: 2,
-    nextCenter: [1,1.5]
-  },
-  j: {
-    positions: [[0,0,1,2], [0,1,1,1]],
-    box: [3,3],
-    origin: 'center',
-    rotationOrigin: [1,1],
-    rotations: 4,
-    nextCenter: [1,1.5]
-  },
-  l: {
-    positions: [[2,0,1,2], [0,1,1,1]],
-    box: [3,3],
-    origin: 'center',
-    rotationOrigin: [1,1],
-    rotations: 4,
-    nextCenter: [1,1.5]
-  },
-  i: {
-    positions: [[0,1,2,3], [0,0,0,0]],
+  }),
+  j: merge(defaultAttrs(), {
+    positions: [
+      [0,0,1,2],
+      [0,1,1,1]
+    ],
+  }),
+  l: merge(defaultAttrs(), {
+    positions: [
+      [2,0,1,2],
+      [0,1,1,1]
+    ],
+  }),
+  i: merge(defaultAttrs(), {
+    positions: [
+      [0,1,2,3],
+      [0,0,0,0]
+    ],
     box: [4,1],
     origin: [1.5, 0.5],
     rotationOrigin: [1,0],
     rotations: 2,
     nextCenter: [0.5,2]
-  },
-  o: {
-    positions: [[0,1,0,1], [0,0,1,1]],
+  }),
+  o: merge(defaultAttrs(), {
+    positions: [
+      [0,1,0,1],
+      [0,0,1,1]
+    ],
     box: [2,2],
-    origin: 'center',
     rotationOrigin: [0.5,0.5],
     rotations: 0,
     nextCenter: [1.5,1.5]
-  }
+  })
 };
